8c7c6639db186312037500e62c71969cd90b841c
Andrew Petriccione <andrew.petriccione@metlife.com>
2019-01-02 11:47:36 -0500

    1st commit to master

---
+++ TestMergingProject/src/MergeTestClass.java
@@ -0,0 +1,9 @@
+
+public class MergeTestClass {
+
+	public static void main(String[] args) {
+		System.out.println("Test message to show how to merge branches--this is the original message on master. ");
+
+	}
+
+}
