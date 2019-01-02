7ea5ffe27038df253eb1e91fd210abe5482edf8c
Andrew Petriccione <andrew.petriccione@metlife.com>
2019-01-02 11:52:11 -0500

    commit to the new branch, and then will try to merge this to master

--- TestMergingProject/src/MergeTestClass.java
+++ TestMergingProject/src/MergeTestClass.java
@@ -2,8 +2,8 @@
 public class MergeTestClass {
 
 	public static void main(String[] args) {
-		System.out.println("Test message to show how to merge branches--this is the original message on master. ");
-
+		System.out.println("Test message, changed in new branch, to show how to merge branches--this is the NOT the original message on master. ");
+		System.out.println("New line added to other branch ");
 	}
 
 }
