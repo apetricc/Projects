722f1ee3447e18e3da7ab524d4e942d101e15e51
Andrew Petriccione <andrew.petriccione@metlife.com>
2019-01-02 14:00:28 -0500

    deleted the custom method

--- TestMergeProj/src/mypackage/MyClass.java
+++ TestMergeProj/src/mypackage/MyClass.java
@@ -2,16 +2,11 @@ package mypackage;
 
 public class MyClass {
 
-	public void makeString() {
-		for (int i = 0; i < 5; i++) {
-			if (i < 6) System.out.println("will this make a conflict?");
-		}
-		
-	}
+	
 	
 	public static void main(String[] args) {
 		MyClass myClass = new MyClass();
-		myClass.makeString();
+	//	myClass.makeString();
 
 	}
 
