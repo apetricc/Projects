1c06828fe56cb2f10bf5a7b203bd290c958a0589
Andrew Petriccione <andrew.petriccione@metlife.com>
2019-01-02 11:58:13 -0500

    hope this works

--- TestMergingProject/src/MergeTestClass.java
+++ TestMergingProject/src/MergeTestClass.java
@@ -2,8 +2,7 @@
 public class MergeTestClass {
 
 	public static void main(String[] args) {
-		System.out.println("Test message, changed in new branch, to show how to merge branches--this is the NOT the original message on master. ");
-		System.out.println("New line added to other branch ");
+		System.out.println("come on, lthis has to trigger conflict, right?");
 	}
 
 }
